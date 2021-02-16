9d197f4d617b48153c9da0cb73328e109a8f6a6c
rasoul.sh <rasoul.shahhoseini@gmail.com>
2021-02-16 12:25:33 +0330

    :construction: post views

---
+++ Amlakbashi.Host/TagHelpers/IsCheckedTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Amlakbashi.Host.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "is-checked")]
+    public class IsCheckedTagHelper : TagHelper
+    {
+        public bool IsChecked { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("checked", IsChecked ? "checked" : "");
+        }
+    }
+}
