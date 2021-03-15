2e0a126eb1f6e4c4d6958f1a758738a8ccfab06d
rasoul.sh <rasoul.shahhoseini@gmail.com>
2021-03-15 14:59:00 +0330

    :tada: move dashboard changes from old project

---
+++ Amlakbashi.Host/wwwroot/Resource/Scripts/dashboard/dashboard-navigation.js
@@ -0,0 +1,17 @@
+﻿
+$(document).click(function () {
+    $('nav.dashboard__navigatoin').addClass("js-show-menu");
+    $('.js-bg').removeClass("bg-show-menu");
+    $('body').css("overflow", "auto");
+})
+$('.js-berger-menu').on('click', function (event) {
+    $('nav.dashboard__navigatoin').toggleClass("js-show-menu");
+    $('.js-bg').toggleClass("bg-show-menu");
+    if ($('nav.dashboard__navigatoin').hasClass("js-show-menu")) {
+        $('nav.dashboard__navigatoin').animate({ scrollTop: 0 });
+    }
+    if (!$('nav.dashboard__navigatoin').hasClass("js-show-menu")) {
+        $('body').css("overflow", "hidden");
+    } 
+    event.stopPropagation();
+})
